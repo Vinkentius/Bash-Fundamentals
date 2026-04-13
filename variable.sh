7115b1626599ffbbe3ece045310faa9e688b2504
Vinkentius <VObiekwe@outlook.com>
2026-04-13 10:14:40 -0500

    Called num1

--- variable.sh
+++ variable.sh
@@ -3,6 +3,8 @@
 num1=32
 num2=52 
 	
-	# These 2 lines set the variables num1 and num2 to 32 and 52 respectively
+	# These 2 lines set the variables num1 and num2 to 32 and 52 respectively, There must be no spaces around the equals sign.
 
-echo num1
+echo $num1
+
+	# Here, we're calling for num1. To call a variable you must put a dollar sign ($) before the variable name.
