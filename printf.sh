c9eb2c1cc72a0c0fb56d6abf20a0cf31e66eefec
Vinkentius <VObiekwe@outlook.com>
2026-04-14 10:43:16 -0500

    Added comments

--- printf.sh
+++ printf.sh
@@ -8,3 +8,12 @@
 # printf [-v var] format [arguments]
 
 printf "Hello %s\nI'm in grade %d\n" "coder" "11"
+
+# Outputs: Hello coder
+#		   I'm in grade 11
+
+# The first quotation marks are for the format. 
+	# 'Hello %s\n' means that printf will output 'Hello' and then a string and then a new line. (%s is for string, and \n is for newline)
+	# 'I'm in grade %d\n' means that printf will output 'I'm in grade' and then an integer and then a new line. (%d is for integer)
+
+# The next 2 quotation marks are the arguments. The arguments go in the same order they are called in the format.
