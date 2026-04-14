64581bb64caf259a872435c5d51087664f98d442
Vinkentius <VObiekwe@outlook.com>
2026-04-14 10:06:49 -0500

    Added test variables and echo statements.

--- quotations.sh
+++ quotations.sh
@@ -3,3 +3,9 @@
 # Quotations in bash are used to remove the special meaning of characters, which makes sure that the shell treats them literally.
 # Single quotes keeps the literal value of the characters within the quotes.
 # Double quotes allows for variable expansion within the quotes.
+
+var=100
+
+echo '$var' 
+
+echo "$var"
