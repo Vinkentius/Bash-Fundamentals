4fa20e2764679c6ddc85d787521f2df2e4b08086
Vinkentius <VObiekwe@outlook.com>
2026-04-14 10:25:18 -0500

    Added printf file and explained printf.

---
+++ printf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# printf and echo are both used to output text into the terminal.
+# However, printf gives you more control over the output format than echo does.
+# printf supports advanced formatting specifiers such as %s and %d
+# This allows for precise text formatting that echo is not capable of.
