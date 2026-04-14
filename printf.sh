e6416f3daf0283769b747472ee6936fe18fede07
Vinkentius <VObiekwe@outlook.com>
2026-04-14 10:37:37 -0500

    Added code

--- printf.sh
+++ printf.sh
@@ -4,3 +4,7 @@
 # However, printf gives you more control over the output format than echo does.
 # printf supports advanced formatting specifiers such as %s and %d
 # This allows for precise text formatting that echo is not capable of.
+
+# printf [-v var] format [arguments]
+
+printf "Hello %s\nI'm in grade %d\n" "coder" "11"
