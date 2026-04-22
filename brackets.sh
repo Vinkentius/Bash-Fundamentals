25eaa011cd528b940e31ab7a68929cbf4efd87b4
Vinkentius <VObiekwe@outlook.com>
2026-04-22 10:03:01 -0500

    Added explanations and comments for single parentheses

--- brackets.sh
+++ brackets.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# There a lot of different types of brackets you can use in Bash scripting. 
+
+# Single Parentheses
+
+# Single parentheses will run the commands inside in a subshell, and any variables declared will disappear after.
+
+num=50
+( num=80; echo $num ) #This will run in a subshell and output 80.
+echo $num 			# This will run in the regular shell and output the original value of the variable.
