6c279aaadee5e64458be192e8018351b12134c97
Vinkentius <VObiekwe@outlook.com>
2026-05-05 10:14:52 -0500

    Added comments explaining if, else, and elif

--- ifelse.sh
+++ ifelse.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+# You use if, elif, or else if you want something to run only if a specific statement returns true.
+
+# elif is short for else if, which basically says "The first statement you passed through was false, so lets check this second statement." With your second statement being your else if statement.
+
+# Normally, when you just use an if statement on its own, if your argument was false, then nothing happens.
+# However, when you use else, if your argument was false, whatever is part of that else statement will run.
