b8d58fcb10fe198e1e70b8a369dbc36eb0242f50
Vinkentius <VObiekwe@outlook.com>
2026-05-06 10:37:56 -0500

    Listed some common flags for the read command.

--- userinput.sh
+++ userinput.sh
@@ -2,3 +2,8 @@
 
 # To ask for user input through the terminal, you use the read command.
 # The read command will allow the user to type whatever they like into the terminal, which you can then store in a variable and use in your code.
+
+# Some flags that are commonly used with the read command go as follows:
+
+# -p  Display a prompt before waiting for input
+# -s  Hide the user input (essential for password entry)
