eca0e59028c6bb497cb0eff59e78cc226566d672
Vinkentius <VObiekwe@outlook.com>
2026-05-06 10:17:05 -0500

    Added comments explaining conditionals.

--- conditionals.sh
+++ conditionals.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# A conditional in bash is when certain pieces of code will only run when a specific condition is returned true.
+
+# For conditionals we use if and case statements.
+# We also use either single square brackets or double square brackets ([], [[]]).
