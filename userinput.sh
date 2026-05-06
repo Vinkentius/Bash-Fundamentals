64b22137e1b3b3f6e8a4c1fd136483363c094df7
Vinkentius <VObiekwe@outlook.com>
2026-05-06 10:33:56 -0500

    Explained the read command.

--- userinput.sh
+++ userinput.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# To ask for user input through the terminal, you use the read command.
+# The read command will allow the user to type whatever they like into the terminal, which you can then store in a variable and use in your code.
