90cb2b126fff9062979a2fc762cbfc9ad165206a
Vinkentius <VObiekwe@outlook.com>
2026-05-07 10:38:00 -0500

    Added code explaining the AND operator

--- extraconditionals.sh
+++ extraconditionals.sh
@@ -2,5 +2,14 @@
 
 # Conditionals can become even more useful when you start using more of the advanced features of conditionals.
 # You can run more complex tests on multiple conditions at a time, and compare all of those conditions, and only run your code if they pass a series of tests of your choosing.
+# Some conditionals only work with specific types of brackets.
 
+# You can combine multiple conditions using '&&', '==', '||', and '!'
 
+	# '&&' is used as the AND operator in conditionals.
+	var1=1
+	var2=1
+
+	if [[ $var1 && $var2 == 1 ]]; then
+		echo "True"
+	fi
