fb5b7963949b0d190debfdd6e021d60e26c886a5
Vinkentius <VObiekwe@outlook.com>
2026-05-07 10:20:27 -0500

    Explained the use of logical operators in conditionals

--- extraconditionals.sh
+++ extraconditionals.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# Conditionals can become even more useful when you start using more of the advanced features of conditionals.
+# You can run more complex tests on multiple conditions at a time, and compare all of those conditions, and only run your code if they pass a series of tests of your choosing.
+
+
