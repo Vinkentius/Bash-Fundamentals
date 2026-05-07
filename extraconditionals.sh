1360b75617f052bf359685b399d569d66de981f9
Vinkentius <VObiekwe@outlook.com>
2026-05-07 10:45:08 -0500

    Added code explaining the OR operator

--- extraconditionals.sh
+++ extraconditionals.sh
@@ -7,19 +7,27 @@
 # You can combine multiple conditions using '&&', '==', '||', and '!'
 
 	# '&&' is used as the AND operator in conditionals.
-	var1=1
-	var2=1
-	var3=0
+		var1=1
+		var2=1
+		var3=0
 
-	if [[ $var1 && $var2 == 1 ]]; then # Since var1 and var2 BOTH have a value of 1, then this code will return true.
-		echo "True"
-	else
-		echo "False"
-	fi
+		if [[ $var1 && $var2 == 1 ]]; then # Since var1 and var2 BOTH have a value of 1, then this code will return true.
+			echo "True"
+		else
+			echo "False"
+		fi
 
 
-	if [[ $var1 && $var2 && $var3 == 1 ]]; then # But now, even though var1 and var2 are still equal, var3 isn't. Therefore, this code will return false.
-		echo "True"
-	else
-		echo "False"
-	fi
+		if [[ $var1 && $var2 && $var3 == 1 ]]; then # But now, even though var1 and var2 are still equal, var3 isn't. Therefore, this code will return false.
+			echo "True"
+		else
+			echo "False"
+		fi
+	
+	# '||' is used as the OR operator in conditionals.
+
+		if [[ $var1 || $var3 == 1 ]]; then
+			echo "True"
+		else
+			echo "False"
+		fi
