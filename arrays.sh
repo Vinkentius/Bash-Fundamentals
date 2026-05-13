304526e10e783d5280397b4a63ba99dc519c3b39
Vinkentius <VObiekwe@outlook.com>
2026-05-13 10:37:45 -0500

    Explained arrays

--- arrays.sh
+++ arrays.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# An array allows you to store multiple values in a single variable, making managing that data much easier.
+# It's important to keep in mind that the first value in an array is considered the 0th in the index
