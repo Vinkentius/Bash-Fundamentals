c22d8afd673e4cfe1667eccecb55b9d76476b30a
Vinkentius <VObiekwe@outlook.com>
2026-05-19 10:08:42 -0500

    Explained how to create an array

--- arrays.sh
+++ arrays.sh
@@ -2,3 +2,8 @@
 
 # An array allows you to store multiple values in a single variable, making managing that data much easier.
 # It's important to keep in mind that the first value in an array is considered the 0th in the index
+
+# To create an array in bash, you must do the following:
+array=("value1" "value2" "value3")
+
+# Seperate different values in an array with SPACES NOT COMMAS
