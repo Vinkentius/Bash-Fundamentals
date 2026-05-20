07220a3604ac309948130c779fa20ef92775600f
Vinkentius <VObiekwe@outlook.com>
2026-05-20 10:28:41 -0500

    Explained function uses and showed function syntax

--- functions.sh
+++ functions.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Functions in bash let you group commands into easy-to-digest blocks. They can make documentation easier, while also helping to organize scripts and avoid hardcoding.
+
+# Functions are initialized like this.
+
+newfunc() {
+	echo "This is my first function!"
+}
