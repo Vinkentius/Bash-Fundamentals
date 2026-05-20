40052f0c48efd7201a8868ea5a0a8b2cc527fb5c
Vinkentius <VObiekwe@outlook.com>
2026-05-20 10:10:17 -0500

    Added troubleshooting guide for arrays

--- arrays.sh
+++ arrays.sh
@@ -13,7 +13,7 @@ array=("value1" "value2" "value3")
 
 # To access information in an array, you must do the following:
 
-echo ${array[0]}
+echo "${array[0]}"
 
 # Use curly brackets to specify that you're talking about an array and not a variable
 # Use square brackets to specify the index of the element you're talking about in the array
@@ -23,7 +23,7 @@ echo ${array[0]}
 # There are 2 ways to modify elements of an array. Firstly, you could assign a new value to an already existing index by specifying the index.
 
 array[2]="bettervalue3"
-echo ${array[2]}
+echo "${array[2]}"
 
 # This will change the 2nd index to bettervalue3 instead of value3.
 # ( This code will output: bettervalue3 )
@@ -31,9 +31,20 @@ echo ${array[2]}
 # Secondly, you could append a new element to the array
 
 array+=("value4")
-echo ${array[@]}
+echo "${array[@]}"
 
 # This will create a new index and assign value4 to that index.
-# The '@' in the index means that it will echo ALL elements in that index.
+# The '@' in the index means that it will echo ALL elements in that array.
 # ( So this code will output: value1 value2 bettervalue3 value4 )
+
+# Common issues and mistakes
+
+# Using spaces around equals sign.
+	
+#	array = (x y z) INCORRECT
+#	array=(x y z) CORRECT
+
+# Bash arrays start at 0 NOT 1.
+
+# Always use quotes when referencing your array.
 
