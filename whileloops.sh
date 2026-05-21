a5c72459132443a3e5c2a39d50137c0a35a4fae8
Vinkentius <VObiekwe@outlook.com>
2026-05-21 12:48:51 -0500

    Explained while loops and their syntax

--- whileloops.sh
+++ whileloops.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# While loops are used to run commands while a condition a true.
+# The condition is checked again once all of the commands are done running. If the condition is still true, then all the commands will run again (That's why its called a loop)
+
+# Syntax:
+
+# while [condition]; do
+#	commands
+# done
