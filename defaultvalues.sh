e0d2ad56201ee5bfc0df9a07294e2b5446b505a7
Vinkentius <VObiekwe@outlook.com>
2026-05-21 12:25:39 -0500

    Explained how to use ':=' with default values using code and comments. Also expanded on last commit

--- defaultvalues.sh
+++ defaultvalues.sh
@@ -19,7 +19,21 @@ echo "${var:-default}"
 	unset x
 
 	echo "${x:-default}" # This line will output default because the variable x has already been unset, so the code calls for the default value
+	# Additionally, since we used the ':-', x isn't assigned to default value; it's only set to that for that command.
+
+	echo "$x" # This will still output 0, because we used ':=' and only temporarily set x to default.
 
 # Use ':=' to permenantly assign that default value to the variable if it is unset or empty.
+	y=1
+
+	echo "${y:=default}" # Because y is not unset or empty, this will just output the original value for y(1)
+
+	unset y
+
+	echo "${y:=default}" # Now, because y is unset, this will output default.
+	# Additionally, since we used ':=' and not ':-', the value of y is now permenantly assigned to 'default'.
+
+	echo "$y" # This will now output 'default' instead of '1'
+
 # Use ':?' to output an error message if a variable is unset or empty.
 # Use ':+' to use a default value if the variable has a value, if it doesn't have a value then use nothing
