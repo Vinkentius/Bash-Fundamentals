fa166c2f35a39c27790618f144892c355a380525
Vinkentius <VObiekwe@outlook.com>
2026-05-21 10:45:40 -0500

    Explained how to use ':-' wtih default values using code and comments

--- defaultvalues.sh
+++ defaultvalues.sh
@@ -12,6 +12,14 @@ echo "${var:-default}"
 
 # Changing the symbols between the variable and the default value changes what it does/checks for.
 # Use ':-' if you only wish to use the default value for that one command if it is unset or empty
+	x=0
+	
+	echo "${x:-default}"  # This line will output the original value of x (0) because the variable is not unset or empty so there is no need for the default value.
+
+	unset x
+
+	echo "${x:-default}" # This line will output default because the variable x has already been unset, so the code calls for the default value
+
 # Use ':=' to permenantly assign that default value to the variable if it is unset or empty.
 # Use ':?' to output an error message if a variable is unset or empty.
 # Use ':+' to use a default value if the variable has a value, if it doesn't have a value then use nothing
