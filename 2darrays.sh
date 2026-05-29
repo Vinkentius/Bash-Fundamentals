ee156f807c56e387482281ef697faa343a228684
Vinkentius <VObiekwe@outlook.com>
2026-05-29 09:55:03 -0500

    Explained what a 2D array is

--- 2darrays.sh
+++ 2darrays.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# Bash doesn't support 2 dimensional arrays, but there are ways to simulate a 2 dimensional array using 1 dimensional arrays.
+# A 2 dimensional array is like a regular array, but instead of having 1 input for an index, it takes 2 (rows, columns).
