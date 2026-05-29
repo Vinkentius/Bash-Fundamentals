c13137b61002eacb1c00f5c144ea262bd4d2efe1
Vinkentius <VObiekwe@outlook.com>
2026-05-29 10:08:35 -0500

    Explained how to create and assign values to a 2D array

--- 2darrays.sh
+++ 2darrays.sh
@@ -2,3 +2,15 @@
 
 # Bash doesn't support 2 dimensional arrays, but there are ways to simulate a 2 dimensional array using 1 dimensional arrays.
 # A 2 dimensional array is like a regular array, but instead of having 1 input for an index, it takes 2 (rows, columns).
+
+# To create a 2D array, first we declare our array
+
+declare -A matrix # The '-A' flag makes our array associative. This means that our arrays doesn't use numbers starting from 0, it uses custom strings which allows us to use coordinates.
+
+# Then we assign our values
+
+matrix[0,0]="Row 0, Column 0"
+matrix[0,1]="Row 1, Column 0"
+matrix[1,0]="Row 0, Column 1"
+matrix[1,1]="Row 1, Column 1"
+
