b8adb12c22e3b735dd92eb7ff8e176d0a9477a0c
Vinkentius <VObiekwe@outlook.com>
2026-05-29 10:13:01 -0500

    Used 2D arrays in an example

--- 2darrays.sh
+++ 2darrays.sh
@@ -14,3 +14,9 @@ matrix[0,1]="Row 1, Column 0"
 matrix[1,0]="Row 0, Column 1"
 matrix[1,1]="Row 1, Column 1"
 
+# Now we can call on our values in our matrix.
+
+echo "${matrix[0,0]}" # Outputs: Row 0, Column 0
+echo "${matrix[0,1]}" # Outputs: Row 1, Column 0
+echo "${matrix[1,0]}" # Outputs: Row 0, Column 1
+echo "${matrix[1,1]}" # Outputs: Row 1, Column 1
