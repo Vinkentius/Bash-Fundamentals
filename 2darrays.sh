9ae9ff5291295b99b9b982e519fff24aac965ae8
Vinkentius <VObiekwe@outlook.com>
2026-05-29 10:17:53 -0500

    Added troubleshooting guide for 2D arrays

--- 2darrays.sh
+++ 2darrays.sh
@@ -20,3 +20,12 @@ echo "${matrix[0,0]}" # Outputs: Row 0, Column 0
 echo "${matrix[0,1]}" # Outputs: Row 1, Column 0
 echo "${matrix[1,0]}" # Outputs: Row 0, Column 1
 echo "${matrix[1,1]}" # Outputs: Row 1, Column 1
+
+# Common issues and mistakes:
+
+# Missing the '-A' flag when declaring your array will make it so that you won't be able to assign values to a custom string (in this case, coordinates)
+
+# declare matrix INCORRECT
+# declare -A matrix CORRECT
+
+# You must also use quotation marks when assigning your values, and when calling on specific values.
